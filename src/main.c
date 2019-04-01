f8314d6df0d695ce7416e26f03c4e27f20be4b36
Gliulei <1245332635@qq.com>
2019-04-01 12:38:28 +0800

    c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-struct student {
-    int age;
-    char *name;
-} student1;
 
-
+int add_range(int low, int high) {
+    int i,sum;
+    for(i=0;i<=high;i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
 int main(void)
 {
-    char name[] = "xiao ming hehe zhong hua ren ming gong he guo";
-    student1.name = name;
-    printf("%s\n", student1.name);
-    printf("%lu\n", sizeof(name));
-    printf("%lu\n", sizeof(student1));
+    int result[100];
+    result[0] = add_range(1,10);
+    result[1] = add_range(1,100);
+    printf("result[0]=%d\nresult[1]=%d\n", result[0], result[1]);
+    return 0;
 }
